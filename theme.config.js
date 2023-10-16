871c25b3b4350374e767ba1ea8c7e62a30be7f68
csehg0312 <62523554+csehg0312@users.noreply.github.com>
2023-10-16 21:09:03 +0200

    added watermark

--- theme.config.js
+++ theme.config.js
@@ -4,7 +4,7 @@ export default {
   footer: (
     <footer>
       <small>
-        <time>{YEAR}</time> © Your Name.
+        <time>{YEAR}</time> © Gabriel Cseh.
         <a href="/feed.xml">RSS</a>
       </small>
       <style jsx>{`
